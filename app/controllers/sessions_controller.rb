873b68901fa5e5ec0d2f95e396b91d73efaf875f
greicens <gsilva@mail.ccsf.edu>
2017-01-27 20:37:24 -0800

    adds helper module

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user_params = params.require(:user).permit(:email, :password)
+    @user = User.confirm(user_params)
+    if @user
+      login_url(@user)
+      redirect_to @user
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
